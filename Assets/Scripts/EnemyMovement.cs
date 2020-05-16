8cedc6d80a4186a19f3e873c624a4f178bc17a34
arr19422 <arr19422@uvg.edu.gt>
2020-05-15 22:06:13 -0600

    Lab completo

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public GameObject[] wayPointsArray;
+
+    private NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        agent.autoBraking = false;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent && agent.remainingDistance < 0.5f)
+        {
+            agent.SetDestination(wayPointsArray[Random.Range(0, wayPointsArray.Length)].transform.position);
+        }
+    }
+}
